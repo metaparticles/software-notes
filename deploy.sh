5ede4a9acd6bebdf3e400f28f0760ee332fd6936
Ahmed, G <ghahmed@amazon.com>
2019-04-07 21:02:59 +0530

    Adding ruby env to shell script path

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+export PATH="/usr/local/opt/ruby/bin:$PATH"
 git checkout master
 git pull origin master
 git branch -D gh-pages
 git checkout -b gh-pages
+bundle install --path vendor/bundle
 bundle exec jekyll build -s _source
 rm .gitignore
 rm -rf _source
