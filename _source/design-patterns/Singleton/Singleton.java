50891d5bd5dbd1361d78624776a737b6f8ab831b
alok056 <alok@gozefo.com>
2019-04-11 10:31:45 +0530

    added few dp problems

---
+++ _source/design-patterns/Singleton/Singleton.java
@@ -0,0 +1,12 @@
+package Singleton;
+
+public class Singleton {
+    private static Singleton ourInstance = new Singleton();
+
+    public static Singleton getInstance() {
+        return ourInstance;
+    }
+
+    private Singleton() {
+    }
+}
